76a8961478a92498f7c51d1c04a7f5fca099c14e
Jackle1996 <y.luthiger@gmail.com>
2019-09-11 22:42:53 +0200

    generated typescript files based on protobuf api

---
+++ frontend/src/app/grpc_generated/blogposts_pb.d.ts
@@ -0,0 +1,153 @@
+// package: travelbob.blogs
+// file: blogposts.proto
+
+import * as jspb from "google-protobuf";
+
+export class AllBlogsRequest extends jspb.Message {
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): AllBlogsRequest.AsObject;
+  static toObject(includeInstance: boolean, msg: AllBlogsRequest): AllBlogsRequest.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: AllBlogsRequest, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): AllBlogsRequest;
+  static deserializeBinaryFromReader(message: AllBlogsRequest, reader: jspb.BinaryReader): AllBlogsRequest;
+}
+
+export namespace AllBlogsRequest {
+  export type AsObject = {
+  }
+}
+
+export class AllBlogsReply extends jspb.Message {
+  clearBlogsList(): void;
+  getBlogsList(): Array<Blog>;
+  setBlogsList(value: Array<Blog>): void;
+  addBlogs(value?: Blog, index?: number): Blog;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): AllBlogsReply.AsObject;
+  static toObject(includeInstance: boolean, msg: AllBlogsReply): AllBlogsReply.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: AllBlogsReply, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): AllBlogsReply;
+  static deserializeBinaryFromReader(message: AllBlogsReply, reader: jspb.BinaryReader): AllBlogsReply;
+}
+
+export namespace AllBlogsReply {
+  export type AsObject = {
+    blogsList: Array<Blog.AsObject>,
+  }
+}
+
+export class BlogpostsRequest extends jspb.Message {
+  getBlogid(): number;
+  setBlogid(value: number): void;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): BlogpostsRequest.AsObject;
+  static toObject(includeInstance: boolean, msg: BlogpostsRequest): BlogpostsRequest.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: BlogpostsRequest, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): BlogpostsRequest;
+  static deserializeBinaryFromReader(message: BlogpostsRequest, reader: jspb.BinaryReader): BlogpostsRequest;
+}
+
+export namespace BlogpostsRequest {
+  export type AsObject = {
+    blogid: number,
+  }
+}
+
+export class BlogpostsReply extends jspb.Message {
+  clearBlogpostsList(): void;
+  getBlogpostsList(): Array<Blogpost>;
+  setBlogpostsList(value: Array<Blogpost>): void;
+  addBlogposts(value?: Blogpost, index?: number): Blogpost;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): BlogpostsReply.AsObject;
+  static toObject(includeInstance: boolean, msg: BlogpostsReply): BlogpostsReply.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: BlogpostsReply, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): BlogpostsReply;
+  static deserializeBinaryFromReader(message: BlogpostsReply, reader: jspb.BinaryReader): BlogpostsReply;
+}
+
+export namespace BlogpostsReply {
+  export type AsObject = {
+    blogpostsList: Array<Blogpost.AsObject>,
+  }
+}
+
+export class Blog extends jspb.Message {
+  getId(): number;
+  setId(value: number): void;
+
+  getBlogimageurl(): string;
+  setBlogimageurl(value: string): void;
+
+  getName(): string;
+  setName(value: string): void;
+
+  getAuthor(): string;
+  setAuthor(value: string): void;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): Blog.AsObject;
+  static toObject(includeInstance: boolean, msg: Blog): Blog.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: Blog, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): Blog;
+  static deserializeBinaryFromReader(message: Blog, reader: jspb.BinaryReader): Blog;
+}
+
+export namespace Blog {
+  export type AsObject = {
+    id: number,
+    blogimageurl: string,
+    name: string,
+    author: string,
+  }
+}
+
+export class Blogpost extends jspb.Message {
+  getId(): number;
+  setId(value: number): void;
+
+  getHeaderimageurl(): string;
+  setHeaderimageurl(value: string): void;
+
+  getTitle(): string;
+  setTitle(value: string): void;
+
+  getText(): string;
+  setText(value: string): void;
+
+  getBlogid(): number;
+  setBlogid(value: number): void;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): Blogpost.AsObject;
+  static toObject(includeInstance: boolean, msg: Blogpost): Blogpost.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: Blogpost, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): Blogpost;
+  static deserializeBinaryFromReader(message: Blogpost, reader: jspb.BinaryReader): Blogpost;
+}
+
+export namespace Blogpost {
+  export type AsObject = {
+    id: number,
+    headerimageurl: string,
+    title: string,
+    text: string,
+    blogid: number,
+  }
+}
+
