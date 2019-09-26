81e1643ef5f4777d47ac1886d740ff1f6646a0b3
Jackle1996 <y.luthiger@gmail.com>
2019-09-26 16:28:22 +0200

    extended blogs service to fetch blogposts

--- frontend/src/app/blog.service.ts
+++ frontend/src/app/blog.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { Blog, Timestamp, AllBlogsRequest, AllBlogsReply, BlogpostsRequest, BlogpostsReply } from '../../../api/grpc-web-ts/blogposts_pb';
+import { Blog, AllBlogsRequest, AllBlogsReply } from '../../../api/grpc-web-ts/blogposts_pb';
+import { Blogpost, Timestamp, BlogpostsRequest, BlogpostsReply } from '../../../api/grpc-web-ts/blogposts_pb';
 import { BlogsAPIClient } from '../../../api/grpc-web-ts/BlogpostsServiceClientPb';
 import { Error } from 'grpc-web';
 
@@ -7,18 +8,35 @@ import { Error } from 'grpc-web';
   providedIn: 'root'
 })
 export class BlogService  {
-  blogs: Blog[];
+  private grpcClient: BlogsAPIClient;
+  private blogs: Blog[];
+  private blogposts: Blogpost[];
 
   constructor() {
-    const grpcClient: BlogsAPIClient = new BlogsAPIClient('http://localhost:8080', null, null);
-    grpcClient.getAllBlogs(new AllBlogsRequest(), {}, (err: Error | null, response: AllBlogsReply) => {
-      if (err) { console.log('Error!!!: ', err); }
+    this.grpcClient = new BlogsAPIClient('http://localhost:8080', null, null);
+
+    this.grpcClient.getAllBlogs(new AllBlogsRequest(), {}, (err: Error | null, response: AllBlogsReply) => {
+      if (err) { console.log('getAllBlogs Error:: ', err); }
       this.blogs = response.getBlogsList();
-      console.log(this.blogs);
     });
+    console.log('blogs=', this.blogs);
   }
 
   getAllBlogs(): Blog[] {
     return this.blogs;
+  }
+
+  getAllBlogposts(): Blogpost[] {
+    return this.blogposts;
+  }
+
+  getBlogPosts(blogId: number, callback): void {
+    const request = new BlogpostsRequest();
+    request.setBlogid(blogId);
+
+    this.grpcClient.getBlogposts(request, {}, (err: Error | null, response: BlogpostsReply) => {
+      if (err) { console.log('getBlogposts Error:: ', err); }
+      this.blogposts = response.getBlogpostsList();
+    }).on('data', data => { callback(data.getBlogpostsList()); });
   }
 }
--- frontend/src/app/blogview/blogview.component.ts
+++ frontend/src/app/blogview/blogview.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { BlogService } from '../blog.service';
+import { Blogpost } from '../../../../api/grpc-web-ts/blogposts_pb';
 
 @Component({
   selector: 'app-blogview',
   templateUrl: './blogview.component.html',
   styleUrls: ['./blogview.component.css']
 })
-export class BlogviewComponent implements OnInit {
 
-  constructor() { }
+export class BlogviewComponent implements OnInit {
+  private blogId: number;
+  private blogPosts: Blogpost[];
+
+  constructor(private route: ActivatedRoute, public blogService: BlogService) {
+    this.blogPosts = [];
+    this.route.paramMap.subscribe(params => {
+       this.blogId = Number(params.get('id'));
+       console.log('blogId=', this.blogId)
+    });
+    this.blogService.getBlogPosts(this.blogId, (posts: Blogpost[]) => this.assignBlogposts(posts));
+  }
 
   ngOnInit() {
   }
 
+  getAllBlogposts(): Blogpost[] {
+    return this.blogPosts;
+  }
+
+  private assignBlogposts(posts: Blogpost[]) {
+    console.log('blogposts= ', posts);
+    this.blogPosts = posts;
+  }
 }
