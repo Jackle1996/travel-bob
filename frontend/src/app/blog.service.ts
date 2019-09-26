1355dc2252bf128259da5677c8ecc29036bbae25
Jackle1996 <y.luthiger@gmail.com>
2019-09-26 11:12:21 +0200

    frontend pulls blogs from server now

--- frontend/src/app/blog.service.ts
+++ frontend/src/app/blog.service.ts
@@ -14,41 +14,13 @@ export class BlogService  {
     // TODO: use this, when node server is implemented
     console.log('getAllBlogs..');
     grpcClient.getAllBlogs(new AllBlogsRequest(), {}, (err: Error | null, response: AllBlogsReply) => {
-      if (err) { console.log(err); }
+      if (err) { console.log('Error!!!: ', err); }
       this.blogs = response.getBlogsList();
       console.log(this.blogs);
     });
   }
 
-  getAllBlogsDummy(): Blog[] {
-    const newBlogs: Blog[] = [];
-
-    const startDate: Timestamp = new Timestamp();
-    const endDate: Timestamp = new Timestamp();
-
-    startDate.setSeconds(1567347502);
-    endDate.setSeconds(1568643502);
-
-
-    const blog1 = new Blog();
-    blog1.setAuthor('Yannick');
-    blog1.setDescription('This is Yannicks super duper blog.');
-    blog1.setBlogimageurl('https://avatars0.githubusercontent.com/u/24352681?s=400&u=b2f9d5516ea3f39f934942e98d0ab1018f690480&v=4');
-    blog1.setId(1);
-    blog1.setDestination('Spain');
-    blog1.setStartdate(startDate);
-    blog1.setEnddate(endDate);
-    newBlogs.push(blog1);
-
-    const blog2 = new Blog();
-    blog2.setAuthor('Marco');
-    blog2.setDescription('Marco wrote this Blog while totally sober. Nice content ahead!');
-    blog2.setBlogimageurl('https://avatars2.githubusercontent.com/u/5302085?s=400&v=4');
-    blog2.setId(2);
-    blog2.setDestination('Uganda');
-    blog2.setStartdate(startDate);
-    blog2.setEnddate(endDate);
-    newBlogs.push(blog2);
-    return newBlogs;
+  getAllBlogs(): Blog[] {
+    return this.blogs;
   }
 }
