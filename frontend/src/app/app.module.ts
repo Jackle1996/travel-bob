8905eea303e6a116015af24a3e3dc97e4db3a713
Jackle1996 <y.luthiger@gmail.com>
2019-09-27 20:29:08 +0200

    added button and service function for createblog

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,20 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+// blog stuff
 import { AppComponent } from './app.component';
 import { BlogviewComponent } from './blogview/blogview.component';
 import { BlogsummaryComponent } from './blogsummary/blogsummary.component';
+import { BlogpostviewComponent } from './blogpostview/blogpostview.component';
 // angular material
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
-
 // flex layout for responsive design
 import { FlexLayoutModule } from '@angular/flex-layout';
+// routing module
 import { AppRoutingModule } from './app-routing/app-routing.module';
-import { BlogpostviewComponent } from './blogpostview/blogpostview.component';
 
 
 @NgModule({
