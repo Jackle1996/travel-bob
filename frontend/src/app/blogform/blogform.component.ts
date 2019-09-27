1177c1ab194a993a47b009d864ba4407f76fe2a7
Jackle1996 <y.luthiger@gmail.com>
2019-09-27 23:30:08 +0200

    blogform is now able ot create/edit blogposts

--- frontend/src/app/blogform/blogform.component.ts
+++ frontend/src/app/blogform/blogform.component.ts
@@ -1,19 +1,62 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
+import { Blog, Timestamp } from '../../../../api/grpc-web-ts/blogposts_pb';
 
 @Component({
   selector: 'app-blogform',
   templateUrl: './blogform.component.html',
   styleUrls: ['./blogform.component.css']
 })
-export class BlogformComponent implements OnInit {
 
+export class BlogformComponent implements OnInit {
+  @ViewChild('blogtitle', null) blogtitle: ElementRef;
+  @ViewChild('destination', null) destination: ElementRef;
+  @ViewChild('startDate', null) startDate: ElementRef;
+  @ViewChild('endDate', null) endDate: ElementRef;
+  @ViewChild('description', null) description: ElementRef;
+
+  private formtitle: string;
   constructor(private dialogRef: MatDialogRef<BlogformComponent>) {}
 
   ngOnInit() {
   }
 
-  close() {
-    this.dialogRef.close('close weas called yeah');
+  getFormTitle() {
+    return this.formtitle;
+  }
+
+  setFormTitle(title: string) {
+    this.formtitle = title;
+  }
+
+  setEditValues(blog: Blog) {
+    this.blogtitle.nativeElement.value = blog.getTitle();
+    this.destination.nativeElement.value = blog.getDestination();
+    const startDateFormat = new Date(blog.getStartdate().getSeconds() * 1000);
+    this.startDate.nativeElement.value = `${startDateFormat.getDate()}.${startDateFormat.getMonth()}.${startDateFormat.getFullYear()}`;
+    const endDateFormat = new Date(blog.getEnddate().getSeconds() * 1000);
+    this.endDate.nativeElement.value = `${endDateFormat.getDate()}.${endDateFormat.getMonth()}.${endDateFormat.getFullYear()}`;
+    this.description.nativeElement.value = blog.getDescription();
+  }
+
+  saveClicked() {
+    const blog: Blog = new Blog();
+    blog.setTitle(this.blogtitle.nativeElement.value);
+    blog.setDestination(this.destination.nativeElement.value);
+
+    const startDateSplit = this.startDate.nativeElement.value.split('.');
+    const startStamp = new Timestamp();
+    startStamp.setSeconds(new Date(startDateSplit[2], startDateSplit[1], startDateSplit[0]).getTime() / 1000);
+    blog.setStartdate(startStamp);
+
+    const endDateSplit = this.endDate.nativeElement.value.split('.');
+    const endStamp = new Timestamp();
+    endStamp.setSeconds(new Date(endDateSplit[2], endDateSplit[1], endDateSplit[0]).getTime() / 1000);
+    blog.setEnddate(endStamp);
+
+    blog.setDescription(this.description.nativeElement.value);
+    // TODO: determine author per login later
+    blog.setAuthor('TODO determine per login later');
+    this.dialogRef.close(blog);
   }
 }
