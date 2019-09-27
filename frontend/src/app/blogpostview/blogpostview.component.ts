066b32a091c3be763b36a393aa3d1e50ba5a00ef
Jackle1996 <y.luthiger@gmail.com>
2019-09-27 18:07:20 +0200

    fetch blogpostid from route

--- frontend/src/app/blogpostview/blogpostview.component.ts
+++ frontend/src/app/blogpostview/blogpostview.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { BlogService } from '../blog.service';
+import { Blogpost } from '../../../../api/grpc-web-ts/blogposts_pb';
 
 @Component({
   selector: 'app-blogpostview',
@@ -6,8 +9,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./blogpostview.component.css']
 })
 export class BlogpostviewComponent implements OnInit {
+  private blogPost: Blogpost;
+  private postId: number;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, public blogService: BlogService) {
+    this.route.paramMap.subscribe(params => {
+       this.postId = Number(params.get('postid'));
+       console.log('postid=', this.postId);
+    });
+
+    // TODO: this.blogService.getBlogPost(this.postId, (post: Blogpost) => this.assignBlogposts(posts));
+  }
 
   ngOnInit() {
   }
