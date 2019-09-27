8510f63a789e83ad62f9564ad5c10d5b1455143a
Jackle1996 <y.luthiger@gmail.com>
2019-09-27 19:46:36 +0200

    adjusted blog service to reload Blogs on summaryview

--- frontend/src/app/blog.service.ts
+++ frontend/src/app/blog.service.ts
@@ -9,25 +9,15 @@ import { Error } from 'grpc-web';
 })
 export class BlogService  {
   private grpcClient: BlogsAPIClient;
-  private blogs: Blog[];
-  private blogposts: Blogpost[];
 
   constructor() {
     this.grpcClient = new BlogsAPIClient('http://localhost:8080', null, null);
+  }
 
+  getBlogs(callback) {
     this.grpcClient.getAllBlogs(new AllBlogsRequest(), {}, (err: Error | null, response: AllBlogsReply) => {
       if (err) { console.log('getAllBlogs Error:: ', err); }
-      this.blogs = response.getBlogsList();
-      console.log('blogs=', this.blogs);
-    });
-  }
-
-  getAllBlogs(): Blog[] {
-    return this.blogs;
-  }
-
-  getAllBlogposts(): Blogpost[] {
-    return this.blogposts;
+    }).on('data', data => { callback(data.getBlogsList()); });
   }
 
   getBlogPosts(blogId: number, callback): void {
@@ -36,7 +26,6 @@ export class BlogService  {
 
     this.grpcClient.getBlogposts(request, {}, (err: Error | null, response: BlogpostsReply) => {
       if (err) { console.log('getBlogposts Error:: ', err); }
-      this.blogposts = response.getBlogpostsList();
     }).on('data', data => { callback(data.getBlogpostsList()); });
   }
 }
--- frontend/src/app/blogsummary/blogsummary.component.ts
+++ frontend/src/app/blogsummary/blogsummary.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { BlogService } from '../blog.service';
+import { Blog } from '../../../../api/grpc-web-ts/blogposts_pb';
 
 @Component({
   selector: 'app-blogsummary',
@@ -7,9 +8,21 @@ import { BlogService } from '../blog.service';
   styleUrls: ['./blogsummary.component.css']
 })
 export class BlogsummaryComponent implements OnInit {
+  private blogs: Blog[];
+
   constructor(public blogService: BlogService) {
+    this.blogService.getBlogs((posts: Blog[]) => this.assignBlogs(posts));
   }
 
   ngOnInit() {
+  }
+
+  getAllBlogs(): Blog[] {
+    return this.blogs;
+  }
+
+  private assignBlogs(blogs: Blog[]) {
+    console.log('blogs= ', blogs);
+    this.blogs = blogs;
   }
 }
