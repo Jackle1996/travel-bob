bb22d36821b98cdc27f8b52019cdcbfc5ed93e58
Jackle1996 <y.luthiger@gmail.com>
2019-09-28 11:55:48 +0200

    delete blogpost implemented on frontend

--- frontend/src/app/blogview/blogview.component.ts
+++ frontend/src/app/blogview/blogview.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { BlogService } from '../services/blog.service';
+import { BlogpostService } from '../services/blogpost.service';
 import { Blogpost } from '../../../../api/grpc-web-ts/blogposts_pb';
+import { MatDialog, MatDialogRef } from '@angular/material';
+import { DeletedialogComponent } from '../deletedialog/deletedialog.component';
 
 @Component({
   selector: 'app-blogview',
@@ -12,25 +14,49 @@ import { Blogpost } from '../../../../api/grpc-web-ts/blogposts_pb';
 export class BlogviewComponent implements OnInit {
   private blogId: number;
   private blogPosts: Blogpost[];
+  private deleteDialog: MatDialogRef<DeletedialogComponent>;
 
-  constructor(private route: ActivatedRoute, private blogService: BlogService) {
+  constructor(private route: ActivatedRoute, private blogpostService: BlogpostService, private dialog: MatDialog) {
     this.blogPosts = [];
     this.route.paramMap.subscribe(params => {
        this.blogId = Number(params.get('blogid'));
        console.log('blogid=', this.blogId)
     });
-    this.blogService.getBlogPosts(this.blogId, (posts: Blogpost[]) => this.assignBlogposts(posts));
+    this.updateBlogs();
   }
 
   ngOnInit() {
+  }
+
+  updateBlogs() {
+    this.blogpostService.getBlogPosts(this.blogId, (posts: Blogpost[]) => this.assignBlogposts(posts));
   }
 
   getAllBlogposts(): Blogpost[] {
     return this.blogPosts;
   }
 
-  private assignBlogposts(posts: Blogpost[]) {
-    console.log('blogposts= ', posts);
-    this.blogPosts = posts;
+  private assignBlogposts(blogPosts: Blogpost[]) {
+    this.blogPosts = blogPosts;
+  }
+
+  createBlogPost() {
+
+  }
+
+  editBlogPost() {
+
+  }
+
+  deleteBlogPost(blogId: number) {
+    if (this.deleteDialog) {
+      this.deleteDialog.close();
+    }
+    this.deleteDialog = this.dialog.open(DeletedialogComponent, {});
+    this.deleteDialog.afterClosed().subscribe(result => {
+      if (result) {
+        this.blogpostService.deleteBlogPost(blogId, () => this.updateBlogs());
+      }
+    });
   }
 }
