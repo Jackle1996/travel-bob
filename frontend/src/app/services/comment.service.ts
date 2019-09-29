4a1df5ef3a27c3c54b726b28d23ed344457a370f
Jackle1996 <y.luthiger@gmail.com>
2019-09-29 18:00:33 +0200

    comment service implemented

---
+++ frontend/src/app/services/comment.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { Comment, GetCommentsRequest, GetCommentsReply } from '../../../../api/grpc-web-ts/comments_pb';
+import { DeleteCommentReply, DeleteCommentRequest } from '../../../../api/grpc-web-ts/comments_pb';
+import { AddCommentRequest, AddCommentReply } from '../../../../api/grpc-web-ts/comments_pb';
+import { CommentsAPIClient } from '../../../../api/grpc-web-ts/CommentsServiceClientPb';
+import { Error } from 'grpc-web';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CommentService {
+  private grpcClient: CommentsAPIClient;
+
+  constructor() {
+    this.grpcClient = new CommentsAPIClient('http://localhost:8080', null, null);
+   }
+
+   getComments(postId: number, callback): void {
+    const request = new GetCommentsRequest();
+    request.setBlogpostId(postId);
+    this.grpcClient.getComments(request, {}, (err: Error | null, response: GetCommentsReply) => {
+      if (err) { console.log('GetCommentsRequest Error:: ', err); }
+      console.log('response', response);
+    }).on('data', data => { callback(data.getCommentsList()); });
+  }
+
+  addComment(comment: Comment, callback) {
+    const request = new AddCommentRequest();
+    request.setComment(comment);
+    this.grpcClient.addComment(request, {}, (err: Error | null, response: AddCommentReply) => {
+      if (err) { console.log('AddCommentRequest Error:: ', err); }
+      console.log('response', response);
+    }).on('data', data => { callback(); });
+  }
+
+  deleteComment(commentId: number, callback) {
+    const request = new DeleteCommentRequest();
+    request.setCommentId(commentId);
+    this.grpcClient.deleteComment(request, {}, (err: Error | null, response: DeleteCommentReply) => {
+      if (err) { console.log('DeleteCommentRequest Error:: ', err); }
+      console.log('response', response);
+    }).on('data', data => { callback(); });
+  }
+}
