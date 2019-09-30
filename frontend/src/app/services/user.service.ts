9a62fe2bf078295ede6eef3c6f9deaa36b30cef0
Yannick Luthiger <yannick.luthiger@stud.hslu.ch>
2019-09-30 18:56:08 +0200

    user service implemented

---
+++ frontend/src/app/services/user.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { User, CreateNewUserRequest, CreateNewUserReply } from '../../../../api/grpc-web-ts/users_pb';
+import { LogInRequest, LogInReply, VerifyTokenRequest, VerifyTokenReply } from '../../../../api/grpc-web-ts/users_pb';
+import { UsersAPIClient } from '../../../../api/grpc-web-ts/UsersServiceClientPb';
+import { Error } from 'grpc-web';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private grpcClient: UsersAPIClient;
+
+  constructor() {
+    this.grpcClient = new UsersAPIClient('http://localhost:8080', null, null);
+  }
+
+  createUser(user: User, callback) {
+    const request = new CreateNewUserRequest();
+    request.setUser(user);
+    this.grpcClient.createNewUser(request, {}, (err: Error | null, response: CreateNewUserReply) => {
+      if (err) { console.log('CreateNewUserRequest Error:: ', err); }
+      console.log('response', response);
+    }).on('data', data => { callback(); });
+  }
+
+  login(email: string, pass: string, callback) {
+    const request = new LogInRequest();
+    request.setEmail(email);
+    request.setPassword(pass);
+    this.grpcClient.logIn(request, {}, (err: Error | null, response: LogInReply) => {
+      if (err) { console.log('LogInRequest Error:: ', err); }
+      console.log('response', response);
+    }).on('data', data => { callback(data.getJwt()); });
+  }
+
+  verifyToken(jwt: string, callback) {
+    const request = new VerifyTokenRequest();
+    request.setJwt(jwt);
+    this.grpcClient.verifyToken(request, {}, (err: Error | null, response: VerifyTokenReply) => {
+      if (err) { console.log('VerifyTokenRequest Error:: ', err); }
+      console.log('response', response);
+    }).on('data', data => { callback(data.getValid()); });
+  }
+}
